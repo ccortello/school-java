990e43f3d4e1cb6089aa5cd393bf07cc3bbd57a5
ccortello <cody.cortello@gmail.com>
2014-02-03 23:28:46 -0700

    Initial commit

--- CS 2420 - Spring 2014/src/homework04/WarehouseReport.java
+++ CS 2420 - Spring 2014/src/homework04/WarehouseReport.java
@@ -2,6 +2,8 @@ package homework04;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -13,13 +15,26 @@ import java.util.Scanner;
  */
 public class WarehouseReport {
 
+    // initialize lists, sets, and maps to hold relevant information from the file
+    public Map<String, Integer> upcs;
+    public ArrayList<Inventory> warehouses;
+
     public static void main(String[] args) {
+        new WarehouseReport();
+    }
+
+    public WarehouseReport() {
+
+        upcs = null;
+        warehouses = null;
 
         // prompt user for input
         System.out.print("Enter a filename to parse: ");
         Scanner userInput = new Scanner(System.in);
 
-//        debug
+        // initialize file from specified input
+//        debug, for speed
+        System.out.println("");
 //        File dataFile = new File("src/homework04/"+userInput.next());
         File dataFile = new File("src/homework04/data1.txt");
 
@@ -31,13 +46,49 @@ public class WarehouseReport {
         }
 
         while (fileScanner.hasNextLine())
-            processNextLine(fileScanner.nextLine());
+            processNextLine(fileScanner.nextLine().trim());
 
         userInput.close();
         System.out.println("Done!");
     }
 
-    private static void processNextLine(String s) {
-        System.out.println(s);
+    /*
+     * Checks the contents of the given String to determine how to process the data
+     */
+    private void processNextLine(String s) {
+
+        Scanner stringScanner = new Scanner(s);
+        String firstWord = stringScanner.next();
+
+//    	debug
+        System.out.println(firstWord);
+
+        // check if final line
+        if (firstWord.equals("End"))
+            return;
+
+        if (firstWord.equals("FoodItem")) {
+
+            // scan the information from the String
+            stringScanner.next();
+            stringScanner.next();
+            stringScanner.next();
+            Integer upc = stringScanner.nextInt();
+
+            stringScanner.next();
+            stringScanner.next();
+            int shelfLife = stringScanner.nextInt();
+
+            stringScanner.next();
+            String foodName = "";
+            while (stringScanner.hasNext())
+                foodName += stringScanner.next() + " ";
+            foodName = foodName.trim();
+
+//    		debug
+//    		System.out.println("upc: "+upc+", food name: "+foodName+", shelf life: "+shelfLife);
+        }
+
+
     }
 }
