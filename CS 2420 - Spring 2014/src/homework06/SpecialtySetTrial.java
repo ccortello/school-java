d727b199702a5f29024ac71ffbadce68bcb1272c
ccortello <cody.cortello@gmail.com>
2014-02-25 20:29:59 -0700

    update

---
+++ CS 2420 - Spring 2014/src/homework06/SpecialtySetTrial.java
@@ -0,0 +1,11 @@
+package homework06;
+
+/**
+ * Created by Cody on 2/25/14.
+ */
+public class SpecialtySetTrial {
+    public static void main(String[] args) {
+        SpecialtySet<Integer> newSet = new SpecialtySet<Integer>();
+        System.out.println("Done!");
+    }
+}
