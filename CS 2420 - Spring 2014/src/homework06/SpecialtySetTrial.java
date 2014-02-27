435b807ed31d10a069f5f21b12b3f97c926affef
ccortello <cody.cortello@gmail.com>
2014-02-26 19:34:42 -0700

    update

--- CS 2420 - Spring 2014/src/homework06/SpecialtySetTrial.java
+++ CS 2420 - Spring 2014/src/homework06/SpecialtySetTrial.java
@@ -1,12 +1,14 @@
 package homework06;
 
 /**
- * Created by Cody on 2/25/14.
+ * Created by Cody Cortello on 2/25/14.
  */
 public class SpecialtySetTrial {
     public static void main(String[] args) {
         SpecialtySet<Integer> newSet = new SpecialtySet<Integer>();
-
+        newSet.add(123);
+        if (newSet.contains(123))
+            System.out.println("correctly contains 123");
         System.out.println("Done!");
     }
 }
