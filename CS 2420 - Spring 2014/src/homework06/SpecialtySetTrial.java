1dd9fd10a89ad30b9d8a3bede4f2f14ba098d325
ccortello <cody.cortello@gmail.com>
2014-03-01 15:32:21 -0700

    update

--- CS 2420 - Spring 2014/src/homework06/SpecialtySetTrial.java
+++ CS 2420 - Spring 2014/src/homework06/SpecialtySetTrial.java
@@ -60,6 +60,8 @@ public class SpecialtySetTrial {
 
 //        System.out.println("after for loop");
 //        newSet.add(10000);
+
+        intsAdded2 = new TreeSet<E>();
         intsAdded.trim();
         System.out.println("\n\nintsAdded, in order:\n" + intsAdded + "\n");
         System.out.println("Set should be:\t" + compareSet.toString());
