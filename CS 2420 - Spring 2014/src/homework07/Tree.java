0b1d6af5582811fdb014b02c6b1fef2fa3db7c70
ccortello <cody.cortello@gmail.com>
2014-03-18 23:32:31 -0600

    update

--- CS 2420 - Spring 2014/src/homework07/Tree.java
+++ CS 2420 - Spring 2014/src/homework07/Tree.java
@@ -13,38 +13,55 @@ import java.util.Scanner;
  */
 public class Tree {
     private Node root;
-    private Map<Integer, Integer> widths;
+    private Node currentNode;
+    public int currentDepth;
+    private Map<Integer, Integer> maximumWidths;
 
     public Tree(File inputFile) {
+        currentDepth = 0;
         try {
             // create scanner in the passed file
             Scanner s = new Scanner(inputFile);
 
             // substantiate the root of the tree
             s.next();
-            root = new Node(null, s.next().substring(0, s.next().length() - 1));
-//            System.out.println(root.data);
+            String rootString = s.next();
+            root = new Node(null, rootString.substring(0, rootString.length() - 1));
+            currentNode = root;
 
-            // use recursion to populate the children of the root node
+//            System.out.println("root data = "+root.data);
+
+            // scans the input file and substantiates the tree
             while (s.hasNext()) {
                 String currentString = s.next();
-                System.out.println(currentString);
-                if (currentString.startsWith("</")) {
-
-                }
+//                System.out.println(currentString + " | ");
+                if (currentString.startsWith("<") && !currentString.substring(1, 2).equals("/")) { // if the string is an opening tag
+//                    System.out.println("if: "+currentString);
+                    String newNodeString = s.next();
+                    Node addNode = new Node(currentNode, newNodeString.substring(0, newNodeString.length() - 1));
+//                    System.out.println("New Node data = "+addNode.data);
+                    currentNode.children.add(addNode);
+                    currentNode = addNode;
+                } else // otherwise the string is a closing tag - change the current node to its parent
+                    currentNode = currentNode.parent;
             }
+            printTree(root);
             System.out.println("After StringTree while loop");
         } catch (FileNotFoundException e) {
             JOptionPane.showMessageDialog(null, "File not found!");
         }
+    }
 
+    private void printTree(Node inputNode) {
+        for (Node n : inputNode.children)
+            printTree(n);
+        System.out.println(inputNode.data);
     }
 
     /**
      * Represents a single node in
      */
     public class Node {
-        // TODO: determine public or private nature of fields
         public String data;
         public Node parent;
         public ArrayList<Node> children;
