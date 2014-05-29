407b06ec815122ce8f2f95b4ffe55df3e10bc492
ccortello <cody.cortello@gmail.com>
2014-05-29 14:03:49 -0600

    updated

---
+++ CS 2420 - Summer 2014/src/assignment2/OurLibraryGenericTest.java
@@ -0,0 +1,35 @@
+package assignment2;
+
+import junit.framework.TestCase;
+
+public class OurLibraryGenericTest extends TestCase {
+
+    LibraryGeneric<LibraryBookGeneric<PhoneNumber>> testLibraryPhone = new LibraryGeneric<LibraryBookGeneric<PhoneNumber>>();
+    LibraryGeneric<LibraryBookGeneric<String>> testLibraryString = new LibraryGeneric<LibraryBookGeneric<String>>();
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+        testLibraryPhone.add();
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testBookList() throws Exception {
+
+    }
+
+    public void testGetInventoryList() throws Exception {
+
+    }
+
+    public void testGetOrderedByAuthor() throws Exception {
+
+    }
+
+    public void testGetOverdueList() throws Exception {
+
+    }
+}
