ad00b06612d1adaa3d4b45ddbe583e03651efabd
ccortello <cody.cortello@gmail.com>
2014-05-29 14:20:54 -0600

    updated

--- CS 2420 - Summer 2014/src/assignment2/OurLibraryGenericTest.java
+++ CS 2420 - Summer 2014/src/assignment2/OurLibraryGenericTest.java
@@ -2,27 +2,40 @@ package assignment2;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+
+/**
+ * Unit tests for the methods added to LibraryGeneric
+ *
+ * @author Cody Cortello
+ * @author Casey Nordgran
+ * @version 1.0
+ */
+
 public class OurLibraryGenericTest extends TestCase {
 
-    LibraryGeneric<LibraryBookGeneric<PhoneNumber>> testLibraryPhone = new LibraryGeneric<LibraryBookGeneric<PhoneNumber>>();
-    LibraryGeneric<LibraryBookGeneric<String>> testLibraryString = new LibraryGeneric<LibraryBookGeneric<String>>();
+    LibraryGeneric<String> testLibraryString;
+    LibraryBookGeneric<String> book1, book2, book3, book4, book5;
 
     public void setUp() throws Exception {
         super.setUp();
 
-//        testLibraryPhone.add();
+        testLibraryString = new LibraryGeneric<String>();
+        book1 = new LibraryBookGeneric<String>(1234567890123L, "first author", "first title");
+        book2 = new LibraryBookGeneric<String>(1234567890354L, "second author", "second title");
+        book3 = new LibraryBookGeneric<String>(1234567890687L, "third author", "third title");
+        book4 = new LibraryBookGeneric<String>(1234567890500L, "fourth author", "fourth title");
+        book5 = new LibraryBookGeneric<String>(1234567890501L, "fifth author", "fifth title");
     }
 
     public void tearDown() throws Exception {
-
-    }
-
-    public void testBookList() throws Exception {
-
+        testLibraryString = null;
     }
 
     public void testGetInventoryList() throws Exception {
-
+        ArrayList<LibraryBookGeneric<String>> stringInventoryList = testLibraryString.getInventoryList();
+        System.out.println(stringInventoryList.get(0));
+//        assertEquals(stringInventoryList, "baklk");
     }
 
     public void testGetOrderedByAuthor() throws Exception {
