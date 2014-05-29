75db5aec6ada13b7e871abd85e725c8bc05e058b
ccortello <cody.cortello@gmail.com>
2014-05-29 14:29:16 -0600

    updated

--- CS 2420 - Summer 2014/src/assignment2/OurLibraryGenericTest.java
+++ CS 2420 - Summer 2014/src/assignment2/OurLibraryGenericTest.java
@@ -14,18 +14,29 @@ import java.util.ArrayList;
 
 public class OurLibraryGenericTest extends TestCase {
 
+    // the only fields are a test library (of String type) and books to be added to the library
     LibraryGeneric<String> testLibraryString;
     LibraryBookGeneric<String> book1, book2, book3, book4, book5;
 
     public void setUp() throws Exception {
         super.setUp();
 
+        // create library
         testLibraryString = new LibraryGeneric<String>();
+
+        // substantiate Book fields
         book1 = new LibraryBookGeneric<String>(1234567890123L, "first author", "first title");
         book2 = new LibraryBookGeneric<String>(1234567890354L, "second author", "second title");
         book3 = new LibraryBookGeneric<String>(1234567890687L, "third author", "third title");
         book4 = new LibraryBookGeneric<String>(1234567890500L, "fourth author", "fourth title");
         book5 = new LibraryBookGeneric<String>(1234567890501L, "fifth author", "fifth title");
+
+        // add books to library
+        testLibraryString.add(book1.getIsbn(), book1.getAuthor(), book1.getTitle());
+        testLibraryString.add(book2.getIsbn(), book2.getAuthor(), book2.getTitle());
+        testLibraryString.add(book3.getIsbn(), book3.getAuthor(), book3.getTitle());
+        testLibraryString.add(book4.getIsbn(), book4.getAuthor(), book4.getTitle());
+        testLibraryString.add(book5.getIsbn(), book5.getAuthor(), book5.getTitle());
     }
 
     public void tearDown() throws Exception {
@@ -33,9 +44,12 @@ public class OurLibraryGenericTest extends TestCase {
     }
 
     public void testGetInventoryList() throws Exception {
+        // use getInventoryList() to retrieve a sorted list
         ArrayList<LibraryBookGeneric<String>> stringInventoryList = testLibraryString.getInventoryList();
         System.out.println(stringInventoryList.get(0));
-//        assertEquals(stringInventoryList, "baklk");
+        System.out.println(book1);
+
+
     }
 
     public void testGetOrderedByAuthor() throws Exception {
