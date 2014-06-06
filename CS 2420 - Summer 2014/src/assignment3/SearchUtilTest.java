e979c79b7a85cc2050451250eda89f7075a9f74e
ccortello <cody.cortello@gmail.com>
2014-06-06 01:59:01 -0600

    put in different remove methods for iterator and class

--- CS 2420 - Summer 2014/src/assignment3/SearchUtilTest.java
+++ CS 2420 - Summer 2014/src/assignment3/SearchUtilTest.java
@@ -3,28 +3,42 @@ package assignment3;
 import junit.framework.TestCase;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class SearchUtilTest extends TestCase {
-    ArrayList<Integer> testList;
+    ArrayBasedCollection<Integer> testList;
 
     public void setUp() throws Exception {
         super.setUp();
 
-        testList = new ArrayList<Integer>();
+        testList = new ArrayBasedCollection<Integer>();
     }
 
     public void tearDown() throws Exception {
     }
 
     public void testBinarySearch() throws Exception {
-        int intsAdded = 100, range = 100;
+        int intsAdded = 500, range = 100;
         for (int i = 0; i < intsAdded; i++)
             testList.add((int) (Math.random() * range));
 
+        class intComparator implements Comparator<Integer> {
+            public int compare(Integer left, Integer right) {
+                return left.compareTo(right);
+            }
+        }
+        Comparator<Integer> cmp = new intComparator();
+
+        ArrayList<Integer> sortedList = testList.toSortedList(cmp);
+        System.out.println("sortedList = " + sortedList);
+
         int tries = 50;
         for (int i = 0; i < tries; i++) {
-            int try=(int) (Math.random() * range);
-//            if (SearchUtil.binarySearch(testList, ))
+            int intToTry = (int) (Math.random() * range);
+            if (SearchUtil.binarySearch(sortedList, intToTry, cmp))
+                System.out.println(intToTry + " found!");
+            else
+                System.out.println(intToTry + " not found!");
         }
     }
 }
