88efa00939a5e2661796f3430712c888d5b0744f
ccortello <cody.cortello@gmail.com>
2014-06-18 22:56:43 -0600

    working on 3bi, set fields

--- CS 2420 - Summer 2014/src/assignment5/LinkedStructureTimer.java
+++ CS 2420 - Summer 2014/src/assignment5/LinkedStructureTimer.java
@@ -32,7 +32,6 @@ public class LinkedStructureTimer {
         // substantiate a random (seeded) MyLinkedList to run tests upon
         MyLinkedList<Integer> testList = new MyLinkedList<Integer>();
         int timesToLoop = 10000, maxSize = 20000;
-        long seed = 13489723891023874L;
         Random rand = new Random(seed);
 
         System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nSize\tTime");
@@ -169,7 +168,71 @@ public class LinkedStructureTimer {
      * O(N) expected
      */
     void problem3bi() {
+        // substantiate a random (seeded) MyLinkedList to run tests upon and an ArrayList of added objects from which
+        //  objects can be accessed for the get method
+        MyLinkedList<Integer> testList = new MyLinkedList<Integer>();
+        ArrayList<Integer> availableInts = new ArrayList<Integer>();
+        Random rand = new Random(seed);
 
+        int timesToLoop = 10000, maxSize = 20000;
+
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nSize\tTime");
+
+        // loop through list sizes and find the average time for addFirst on a list of each size, then print the results
+        for (int i = 0; i <= maxSize; i += 500) {
+
+    		/* timing code modified from Peter Jensen's TimingExperiment08.java from his CS 2420 class of Spring 2014 */
+
+            long startTime, midpointTime1, midpointTime2, stopTime;
+
+            // First, spin computing stuff until one second has gone by.
+            // This allows this thread to stabilize.
+
+            startTime = System.nanoTime();
+            while (System.nanoTime() - startTime < 1000000000) {
+                // empty block
+            }
+
+            // Now, run the test.
+            startTime = System.nanoTime();
+
+            for (long j = 0; j < timesToLoop; j++) {
+                // create a MyLinkedList of the current size (the size is set by the outer for loop index 'i')
+                for (int k = 0; k < i; k++)
+                    testList.addFirst(rand.nextInt());
+
+                // reset testList so it's an empty set for the next loop
+                testList = new MyLinkedList<Integer>();
+            }
+
+            // midpointTime1 marks the end of the testing execution.
+            midpointTime1 = System.nanoTime();
+
+            // reset random variable to assure that the execution time for the setup overhead is _exactly_ the same.
+            //  timing note: the statements between midpointTime1 and midpointTime2 do not affect the timing at all.
+            rand = new Random(seed);
+
+            // since resetting rand would necessarily take execution time we establish a new midpointTime in order to
+            //  calculate the overhead alone (separate from the time taken to reset rand)
+            midpointTime2 = System.nanoTime();
+
+            // Calculate the cost of looping and resetting testList
+
+            for (long j = 0; j < timesToLoop; j++) {
+                testList = new MyLinkedList<Integer>();
+            }
+
+            stopTime = System.nanoTime();
+
+            // Compute the time, subtract the cost of running the loop
+            // from the cost of running the loop and sorting the array.
+            // Average it over the number of runs.
+            double averageTime = ((midpointTime1 - startTime) - (stopTime - midpointTime2)) / timesToLoop;
+            averageTime /= 1.0e9;
+
+            // print out tab-delimited results
+            System.out.println(i + "\t" + averageTime);
+        }
     }
 
     /**
