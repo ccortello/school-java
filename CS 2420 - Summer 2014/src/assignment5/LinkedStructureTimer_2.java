4f280384291c22953936e09faf6a0f1c9cd50717
caseyjames <casey.nordgran@gmail.com>
2014-06-20 20:51:39 -0600

    implemented the rest of the necessary timing methods in LinkedStructureTimer_2 performed all necessary tests and recorded all the data we need.

--- CS 2420 - Summer 2014/src/assignment5/LinkedStructureTimer_2.java
+++ CS 2420 - Summer 2014/src/assignment5/LinkedStructureTimer_2.java
@@ -31,7 +31,7 @@ public class LinkedStructureTimer_2 {
 //        linkedRemoveTimer();
 //        arrayRemoveTimer();
 //        pushTimer();
-        popTimer();
+//        popTimer();
 //        peekTimer();
     }
 
@@ -43,11 +43,8 @@ public class LinkedStructureTimer_2 {
         int maxSize = 100000;   // determines right boundary of plot, or MAX inputs 'N'
         Random rand = new Random();  // used to create random lists
 
-        //print info for the max input size and the number of times looping
-        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop);
-
-        // print column headers for the output data
-        System.out.println("\nsize" + "\ttime");
+        //print info for the max input size and the number of times looping, as well as column headers for results
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime");
 
         // testing loops
         for (int i = 0; i <= maxSize; i += 5000) {  // each of these loops accounts for a different input size 'N'
@@ -108,11 +105,8 @@ public class LinkedStructureTimer_2 {
         int maxSize = 100000;   // determines right boundary of plot
         Random rand = new Random(); // used to create random lists
 
-        //print info for the max input size and the number of times looping
-        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop);
-
-        // print column headers for the output data. Input Size, total time, & average time or growth rate
-        System.out.println("\nsize\ttime\tavg");
+        //print info for the max input size and the number of times looping, as well as column headers for results
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavgTime");
 
         // testing loop
         for (int i = 0; i <= maxSize; i += 5000) {   // each of these loops accounts for a different input size 'N'
@@ -170,37 +164,31 @@ public class LinkedStructureTimer_2 {
      * complexity.
      */
     static void linkedGetTimer() {
-        int timesToLoop = 20;  // higher number causes more accurate average time, but takes much much longer
+        int timesToLoop = 20;  // higher number causes more accurate average time, but takes much longer
         int maxSize = 100000;   // determines right boundary of plot
         Random rand = new Random(); // used to create random lists
 
-        //print info for the max input size and the number of times looping
-        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop);
+        //print info for the max input size and the number of times looping, as well as column headers for results
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavgTime");
 
-        // print column headers for the output data
-        System.out.println("\nsize\ttime\tavg");
+        // testing loops,each of these loops accounts for a different input size 'N'
+        for (int i = 0; i <= maxSize; i += 5000) {
+            if (i == 0) i = 1000; // allows i to equal 1000 then 5000 and then even 5000 increments after.
 
-        // testing loops
-        for (int i = 0; i <= maxSize; i += 10000) {  // each of these loops accounts for a different input size 'N'
-
-            // allows i to equal 1000 then 5000 and then even 5000 increments after.
-            if (i == 0) i = 1000;
-            // declare necessary variables and lists for testing
+            // declare necessary variables, or clear existing ones to start fresh
             MyLinkedList<Integer> linkedGetList = new MyLinkedList<Integer>();
             long startTime, midTime, endTime;
-//            int[] indexArray = new int[i];
-            long seed = System.currentTimeMillis();
+            long seed = System.currentTimeMillis();  //use clock as initial starting point for the seed.
             rand.setSeed(seed);
-
-            // add random numbers to linkedGetList until size equals i, this so the get method can be tested.
-            for (int j = 0; j < i; j++) {
-                linkedGetList.addFirst(rand.nextInt(i));
+            seed = System.currentTimeMillis();  //update seed,
+            linkedGetList.clear();
+            for (int j = 0; j < i; j++) {            // add random numbers to linkedGetList until size equals i
+                linkedGetList.addLast(rand.nextInt());
             }
 
             // this while loop runs for a full second to get things warmed up and running before timing starts
             startTime = System.nanoTime();
-            while (System.nanoTime() - startTime < 1e9) { //empty block
-            }
+            while (System.nanoTime() - startTime < 1e9) {/*empty*/}
 
             // startTime and testing start here
             startTime = System.nanoTime();
@@ -210,7 +198,6 @@ public class LinkedStructureTimer_2 {
                     linkedGetList.get(rand.nextInt(i));  //'get' will be called i times for timesToLoop times for a
                 }                                           //good average time for i
             }
-
             midTime = System.nanoTime();   // midTime is set to aid in subtracting overhead
             rand.setSeed(seed);
             for (int j = 0; j < timesToLoop; j++) {     // same loops run again without .get() to determine overhead
@@ -218,7 +205,6 @@ public class LinkedStructureTimer_2 {
                     rand.nextInt(i);
                 }
             }
-
             endTime = System.nanoTime();
 
             // subtract the over head and determine average time for 'i' calls to get.
@@ -235,50 +221,56 @@ public class LinkedStructureTimer_2 {
      * linked list. Expected get(i) runtime is 0(1) or constant time.
      */
     static void arrayGetTimer() {
-        int timesToLoop = 10;  // higher number causes more accurate average time
-        int maxSize = 10000000;   // right boundry is extremely high to show ArrayLists constant complexity with get(i)
+        int timesToLoop = 2500;  // higher number causes more accurate average time, but takes much longer
+        int maxSize = 100000;   // determines right boundary of plot
+        Random rand = new Random(); // used to create random lists
 
-        //print info for the max input size and the number of times looping
-        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop);
-
-        // print column headers for the output data
-        System.out.println("\nsize\ttime\tavg");
+        //print info for the max input size and the number of times looping, as well as column headers for results
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavgTime");
 
         // testing loops
-        for (int i = 0; i <= maxSize; i += 500000) {  // each of these loops accounts for a different input size 'N'
+        for (int i = 0; i <= maxSize; i += 5000) {  // each of these loops accounts for a different input size 'N'
 
-            if (i == 0) i = 1000;             // allows i to equal 1000 then 5000 and then even 5000 increments after.
-            int randNum = 0;
+            if (i == 0) i = 1000;// allows i to equal 1000 then 5000 and then even 5000 increments after.
             // declare necessary variables and lists for testing
             ArrayList<Integer> arrayGetList = new ArrayList<Integer>(i);
             long startTime, midTime, endTime;
+            long seed = System.currentTimeMillis();
 
-            // add random numbers to linkedGetList until size equals i, this so the get method can be tested.
+            /*create a non empty array first or index out of bounds exception will be thrown,
+            then in the loops below each element is reset to random numbers. This is quicker than clear and
+            re-adding new elements to the array.*/
+
             for (int j = 0; j < i; j++) {
-                arrayGetList.add((int) (Math.random() * Integer.MAX_VALUE));
+                arrayGetList.add(1);
             }
 
             // this while loop runs for a full second to get things warmed up and running before timing starts
             startTime = System.nanoTime();
-            while (System.nanoTime() - startTime < 1000000000) { //empty block
+            while (System.nanoTime() - startTime < 1e9) {
             }
 
             // startTime and testing start here
             startTime = System.nanoTime();
+            rand.setSeed(seed);
             for (int j = 0; j < timesToLoop; j++) {
+                for (int h = 0; h < i; h++) {
+                    arrayGetList.set(h, rand.nextInt());
+                }
                 for (int k = 0; k < i; k++) {
-                    randNum = (int) (Math.random() * i);
-                    arrayGetList.get(randNum);  //'get' will be called i times for timesToLoop times for a
-                }                                           //good average time for i
-            }
-
-            midTime = System.nanoTime();   // midTime is set to aid in subtracting overhead
-            for (int j = 0; j < timesToLoop; j++) {     // same loops run again without .get() to determine overhead
-                for (int k = 0; k < i; k++) {
-                    randNum = (int) (Math.random() * i);
+                    arrayGetList.get(rand.nextInt(i)); // use remove method first with the list at right N size
                 }
             }
-
+            midTime = System.nanoTime();   // midTime is set to aid in subtracting overhead
+            rand.setSeed(seed);
+            for (int j = 0; j < timesToLoop; j++) {
+                for (int h = 0; h < i; h++) {
+                    arrayGetList.set(h, rand.nextInt());
+                }
+                for (int k = 0; k < i; k++) {
+                    rand.nextInt(i);            //this is also calculated as it is not part of the get() method
+                }
+            }
             endTime = System.nanoTime();
 
             // subtract the over head and determine average time for 'i' calls to get.
@@ -291,18 +283,15 @@ public class LinkedStructureTimer_2 {
     }
 
     static void linkedRemoveTimer() {
-        int timesToLoop = 10;  // higher number causes more accurate average time, but takes much longer
+        int timesToLoop = 5;  // higher number causes more accurate average time, but takes much longer
         int maxSize = 100000;   // determines right boundary of plot
         Random rand = new Random(); // used to create random lists
 
-        //print info for the max input size and the number of times looping
-        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop);
-
-        // print column headers for the output data
-        System.out.println("\nsize\ttime\tavg");
+        //print info for the max input size and the number of times looping, as well as column headers for results
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavgTime");
 
         // testing loops
-        for (int i = 0; i <= maxSize; i += 10000) {  // each of these loops accounts for a different input size 'N'
+        for (int i = 0; i <= maxSize; i += 5000) {  // each of these loops accounts for a different input size 'N'
 
             if (i == 0) i = 1000;// allows i to equal 1000 then 5000 and then even 5000 increments after.
             // declare necessary variables and lists for testing
@@ -352,18 +341,15 @@ public class LinkedStructureTimer_2 {
     }
 
     static void arrayRemoveTimer() {
-        int timesToLoop = 100;  // higher number causes more accurate average time, but takes much longer
+        int timesToLoop = 20;  // higher number causes more accurate average time, but takes much longer
         int maxSize = 100000;   // determines right boundary of plot
         Random rand = new Random(); // used to create random lists
 
-        //print info for the max input size and the number of times looping
-        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop);
-
-        // print column headers for the output data
-        System.out.println("\nsize\ttime\tavg");
+        //print info for the max input size and the number of times looping, as well as column headers for results
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavgTime");
 
         // testing loops
-        for (int i = 0; i <= maxSize; i += 10000) {  // each of these loops accounts for a different input size 'N'
+        for (int i = 0; i <= maxSize; i += 5000) {  // each of these loops accounts for a different input size 'N'
 
             if (i == 0) i = 1000;// allows i to equal 1000 then 5000 and then even 5000 increments after.
             // declare necessary variables and lists for testing
@@ -417,11 +403,8 @@ public class LinkedStructureTimer_2 {
         int maxSize = 100000;   // determines right boundary of plot
         Random rand = new Random(); // used to create random lists
 
-        //print info for the max input size and the number of times looping
-        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop);
-
-        // print column headers for the output data. Input Size, total time, & average time or growth rate
-        System.out.println("\nsize\ttime\tavg");
+        //print info for the max input size and the number of times looping, as well as column headers for results
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavgTime");
 
         // testing loop
         for (int i = 0; i <= maxSize; i += 5000) {   // each of these loops accounts for a different input size 'N'
@@ -479,49 +462,45 @@ public class LinkedStructureTimer_2 {
         int maxSize = 100000;   // determines right boundary of plot
         Random rand = new Random(); // used to create random lists
 
-        //print info for the max input size and the number of times looping
-        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop);
-
-        // print column headers for the output data
-        System.out.println("\nsize\ttime\tavg");
+        //print info for the max input size and the number of times looping, as well as column headers for results
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavgTime");
 
         // testing loops
         for (int i = 0; i <= maxSize; i += 5000) {  // each of these loops accounts for a different input size 'N'
 
             if (i == 0) i = 1000;// allows i to equal 1000 then 5000 and then even 5000 increments after.
             // declare necessary variables and lists for testing
-            MyStack<Integer> popTimingList = new MyStack<Integer>();
+            MyStack<Integer> popTimingList;
             long startTime, midTime, endTime;
-            int[] randNum = new int[i * timesToLoop];
             long seed = System.currentTimeMillis();
-            rand.setSeed(seed);
-
-            // add random numbers to stack until size equals i, this so the get method can be tested.
-            for (int j = 0; j < i * timesToLoop; j++) {
-                randNum[j] = rand.nextInt(Integer.MAX_VALUE);
-            }
 
             // this while loop runs for a full second to get things warmed up and running before timing starts
             startTime = System.nanoTime();
-            while (System.nanoTime() - startTime < 1e9) { //empty block
+            while (System.nanoTime() - startTime < 1e9) {
             }
 
             // startTime and testing start here
             startTime = System.nanoTime();
-            for (int j = 1; j < timesToLoop + 1; j++) {
-                for (int k = 1; k < i + 1; k++) {
+            rand.setSeed(seed);
+            for (int j = 0; j < timesToLoop; j++) {
+                popTimingList = new MyStack<Integer>();
+                for (int h = 0; h < i; h++) {
+                    popTimingList.push(rand.nextInt());
+                }
+                for (int k = 0; k < i; k++) {
                     popTimingList.pop(); // use remove method first with the list at right N size
-                    popTimingList.push(randNum[j * k]); // add int back, to maintain stack size
                 }
             }
-
             midTime = System.nanoTime();   // midTime is set to aid in subtracting overhead
-            for (int j = 1; j < timesToLoop + 1; j++) {
-                for (int k = 1; k < i + 1; k++) {
-                    popTimingList.push(randNum[j * k]); //reinclude all necessary overhead to subtract
+            rand.setSeed(seed);
+            for (int j = 0; j < timesToLoop; j++) {
+                popTimingList = new MyStack<Integer>();
+                for (int h = 0; h < i; h++) {
+                    popTimingList.push(rand.nextInt());
+                }
+                for (int k = 0; k < i; k++) {
                 }
             }
-
             endTime = System.nanoTime();
 
             // subtract the over head and determine average time for 'i' calls to get.
@@ -534,6 +513,53 @@ public class LinkedStructureTimer_2 {
     }
 
     static void peekTimer() {
-        //TODO: peekTimer
+        int timesToLoop = 9999;  // higher number causes more accurate average time, but takes much longer
+        int maxSize = 100000;   // determines right boundary of plot
+        Random rand = new Random(); // used to create random lists
+        MyStack<Integer> peekTimingList;
+
+        //print info for the max input size and the number of times looping, as well as column headers for results
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavgTime");
+
+        // testing loops
+        for (int i = 0; i <= maxSize; i += 5000) {  // each of these loops accounts for a different input size 'N'
+
+            if (i == 0) i = 1000;// allows i to equal 1000 then 5000 and then even 5000 increments after.
+            // declare necessary variables and lists for testing
+            peekTimingList = new MyStack<Integer>();
+            long startTime, midTime, endTime;
+            long seed = System.currentTimeMillis();
+            rand.setSeed(seed);
+
+            for (int j = 0; j < i; j++) {
+                peekTimingList.push(rand.nextInt());
+            }
+
+            // this while loop runs for a full second to get things warmed up and running before timing starts
+            startTime = System.nanoTime();
+            while (System.nanoTime() - startTime < 1e9) {
+            }
+
+            // startTime and testing start here
+            startTime = System.nanoTime();
+            for (int j = 0; j < timesToLoop; j++) {
+                for (int k = 0; k < i; k++) {
+                    peekTimingList.peek();
+                }
+            }
+            midTime = System.nanoTime();   // midTime is set to aid in subtracting overhead
+            for (int j = 0; j < timesToLoop; j++) {
+                for (int k = 0; k < i; k++) {
+                }
+            }
+            endTime = System.nanoTime();
+
+            // subtract the over head and determine average time for 'i' calls to get.
+            double totalTime = ((midTime - startTime) - (endTime - midTime)) / timesToLoop;
+            double avgTime = totalTime / i;
+            System.out.println(i + "\t" + totalTime + "\t" + avgTime);     // print results
+
+            if (i == 1000) i = 0;  // used to allows 1000 to 5000 then even increments of 5000
+        }
     }
 }
