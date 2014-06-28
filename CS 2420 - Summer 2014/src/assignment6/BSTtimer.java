b72e1911a25d574fd2e420607ba11cbb347ecac9
ccortello <cody.cortello@gmail.com>
2014-06-27 22:16:32 -0600

    timing implementation

--- CS 2420 - Summer 2014/src/assignment6/BSTtimer.java
+++ CS 2420 - Summer 2014/src/assignment6/BSTtimer.java
@@ -13,58 +13,56 @@ public class BSTtimer {
     }
 
     BSTtimer() {
-//        problem3i();
-        bstInsertRandomTime();
+        bstInsertAscendingTime();
+//        bstInsertRandomTime();
     }
 
-//    void problem3i() {
-//        int timesToLoop = 20;  // higher number causes more accurate average time, but takes much longer
-//        int[] sizes = new int[]{100, 1000, 2000, 4000, 8000, 10000, 20000, 50000, 100000};
-//        long startTime, midTime, endTime;
-//
-//        Random rand = new Random(); // used to create random lists
-//        long seed = 783789742385993847L;
-//        rand.setSeed(seed);
-//
-//        //print info for the max input size and the number of times looping, as well as column headers for results
-//        System.out.println("Sizes = " + sizes + ", loops = " + timesToLoop + "\n\nsize\ttime\tavgTime");
-//
-//        // loop through each array size
-//        for (int size : sizes) {  // each of these loops accounts for a different input size 'N'
-//
-//            // declare necessary variables and lists for testing
-//            BinarySearchTree<Integer> testBST = new BinarySearchTree<Integer>(size);
-//            int[] addArray = new int[size];
-//            for (int i : addArray)
-//                addArray[i] = i;
-//
-//            startTime = System.nanoTime();
-//            // this while loop runs for a full second to get things warmed up and running before timing starts
-//            while (System.nanoTime() - startTime < 1e9) {
-//            }
-//
-//            // start the tests
-//            startTime = System.nanoTime();
-//            for (int j = 0; j < timesToLoop; j++) {
-//                for (int addInt : addArray) // add each integer from the array
-//                    testBST.add(addInt);
-//                testBST = new BinarySearchTree<Integer>(size); // then reset the BST for the next loop
-//            }
-//
-//            // find the testing overhead (operations separate from the one being tested)
-//            midTime = System.nanoTime();
-//            for (int j = 0; j < timesToLoop; j++) {
-//                testBST = new BinarySearchTree<Integer>(size);
-//            }
-//
-//            endTime = System.nanoTime();
-//
-//            // subtract the over head and determine average time for 'i' calls to get.
-//            double totalTime = ((midTime - startTime) - (endTime - midTime)) / timesToLoop;
-//            double avgTime = totalTime / size;
-//            System.out.println(size + "\t" + totalTime + "\t" + avgTime);     // print results
-//        }
-//    }
+    void bstInsertAscendingTime() {
+        int timesToLoop = 100;  // higher number causes more accurate average time
+        int maxSize = 100000;   // determines right boundary of plot
+        Random rand = new Random(); // used to create random lists
+
+        //print info for the max input size and the number of times looping, as well as column headers for results
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavg");
+
+        // testing loop
+        for (int i = 1000; i <= maxSize; i += 1000) {   // each of these loops accounts for a different input size 'N'
+
+            BinarySearchTree<Integer> randListBST;
+            long startTime, midTime, endTime;
+            long seed = System.currentTimeMillis();
+            rand.setSeed(seed);
+
+            // let a while loop run for a full second to get things spooled up.
+            startTime = System.nanoTime();
+            while (System.nanoTime() - startTime < 1e9) { //empty block
+            }
+
+            // startTime and testing start here.
+            startTime = System.nanoTime();
+            rand.setSeed(seed);
+            for (int j = 0; j < timesToLoop; j++) {
+                randListBST = new BinarySearchTree<Integer>();
+                for (int k = 0; k < i; k++) {
+                    randListBST.add(k);
+                }
+            }
+
+            midTime = System.nanoTime();
+            rand.setSeed(seed);
+            for (int j = 0; j < timesToLoop; j++) {
+                randListBST = new BinarySearchTree<Integer>();
+                for (int k = 0; k < i; k++) {
+                }
+            }
+            endTime = System.nanoTime();
+
+            // subtract the over head and determine average time for 'i' calls to get.
+            double totalTime = ((midTime - startTime) - (endTime - midTime)) / timesToLoop;
+            double avgTime = totalTime / i;
+            System.out.println(i + "\t" + totalTime + "\t" + avgTime);     // print results
+        }
+    }
 
     void bstInsertRandomTime() {
         int timesToLoop = 1000;  // higher number causes more accurate average time
@@ -72,7 +70,7 @@ public class BSTtimer {
         Random rand = new Random(); // used to create random lists
 
         //print info for the max input size and the number of times looping, as well as column headers for results
-        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavgTime");
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavg");
 
         // testing loop
         for (int i = 0; i <= maxSize; i += 5000) {   // each of these loops accounts for a different input size 'N'
@@ -103,6 +101,7 @@ public class BSTtimer {
             rand.setSeed(seed);
             for (int j = 0; j < timesToLoop; j++) {
                 tempList = new ArrayList<Integer>(intList);
+                randListBST = new BinarySearchTree<Integer>();
                 for (int k = 0; k < i * 5; k++) {        //loops 10 times more to ensure good permuation
                     swapElements(tempList, rand.nextInt(i), rand.nextInt(i));
                 }
@@ -115,6 +114,7 @@ public class BSTtimer {
             rand.setSeed(seed);
             for (int j = 0; j < timesToLoop; j++) {
                 tempList = new ArrayList<Integer>(intList);
+                randListBST = new BinarySearchTree<Integer>();
                 for (int k = 0; k < i * 5; k++) {
                     swapElements(tempList, rand.nextInt(i), rand.nextInt(i));
                 }
