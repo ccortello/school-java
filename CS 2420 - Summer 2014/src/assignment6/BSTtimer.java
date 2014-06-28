8e8bf8b824c0e01e058f48b03d2f2fcf18ddad6a
caseyjames <casey.nordgran@gmail.com>
2014-06-27 20:55:28 -0600

    timing implementation

--- CS 2420 - Summer 2014/src/assignment6/BSTtimer.java
+++ CS 2420 - Summer 2014/src/assignment6/BSTtimer.java
@@ -1,5 +1,6 @@
 package assignment6;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -12,7 +13,8 @@ public class BSTtimer {
     }
 
     BSTtimer() {
-        problem3i();
+//        problem3i();
+        bstInsertRandomTime();
     }
 
     void problem3i() {
@@ -62,5 +64,76 @@ public class BSTtimer {
             double avgTime = totalTime / size;
             System.out.println(size + "\t" + totalTime + "\t" + avgTime);     // print results
         }
+    }
+
+    void bstInsertRandomTime() {
+        int timesToLoop = 100;  // higher number causes more accurate average time
+        int maxSize = 100000;   // determines right boundary of plot
+        Random rand = new Random(); // used to create random lists
+
+        //print info for the max input size and the number of times looping, as well as column headers for results
+        System.out.println("MaxSize = " + maxSize + ", loops = " + timesToLoop + "\n\nsize\ttime\tavgTime");
+
+        // testing loop
+        for (int i = 0; i <= maxSize; i += 5000) {   // each of these loops accounts for a different input size 'N'
+
+            // declare necessary variables and lists for testing
+            ArrayList<Integer> intList = new ArrayList<Integer>(i);
+            ArrayList<Integer> tempList = new ArrayList<Integer>(i);
+            BinarySearchTree<Integer> randListBST = new BinarySearchTree<Integer>();
+            long startTime, midTime, endTime;
+            long seed = System.currentTimeMillis();
+            rand.setSeed(seed);
+
+            // create list if Integers from 1 to 'i'
+            for (int j = 1; j <= i; j++) {
+                intList.add(j);
+            }
+
+            // let a while loop run for a full second to get things spooled up.
+            startTime = System.nanoTime();
+            while (System.nanoTime() - startTime < 1e9) { //empty block
+            }
+
+            // startTime and testing start here.
+            startTime = System.nanoTime();
+            rand.setSeed(seed);
+            for (int j = 0; j < timesToLoop; j++) {
+                tempList = new ArrayList<Integer>(intList);
+                for (int k = 0; k < i * 10; k++) {        //loops 10 times more to ensure good permuation
+                    swapElements(tempList, rand.nextInt(i), rand.nextInt(i));
+                }
+                for (int k = 0; k < i; k++) {
+                    randListBST.add(tempList.get(k));
+                }
+            }
+
+            midTime = System.nanoTime();
+            rand.setSeed(seed);
+            for (int j = 0; j < timesToLoop; j++) {
+                tempList = new ArrayList<Integer>(intList);
+                for (int k = 0; k < i * 10; k++) {
+                    swapElements(tempList, rand.nextInt(i), rand.nextInt(i));
+                }
+                for (int k = 0; k < i; k++) {
+                    tempList.get(k);
+                }
+            }
+            endTime = System.nanoTime();
+
+            // subtract the over head and determine average time for 'i' calls to get.
+            double totalTime = ((midTime - startTime) - (endTime - midTime)) / timesToLoop;
+            double avgTime = totalTime / i;
+            System.out.println(i + "\t" + totalTime + "\t" + avgTime);     // print results
+        }
+    }
+
+    public static <T extends Comparable<? super T>> void swapElements(ArrayList<T> list, int left, int right) {
+        // stored copy of left indexed element to temp
+        T temp = list.get(left);
+        // reassign left indexed element to a copy of the the right indexed element
+        list.set(left, list.get(right));
+        // replace right indexed element with previous value of left index stored in temp
+        list.set(right, temp);
     }
 }
