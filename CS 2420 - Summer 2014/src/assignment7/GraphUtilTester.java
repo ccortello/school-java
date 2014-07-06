2d02344a36d5d1ff2132d285a760461a878f0d29
caseyjames <casey.nordgran@gmail.com>
2014-07-05 18:13:25 -0600

    added isCyclic( ) and hasPath( ) methods.   isCyclic( ) uses hasPath( ), and isCyclic is used in topologicalSort to throw an error if the graph is cyclic.

--- CS 2420 - Summer 2014/src/assignment7/GraphUtilTester.java
+++ CS 2420 - Summer 2014/src/assignment7/GraphUtilTester.java
@@ -16,15 +16,6 @@ public class GraphUtilTester extends TestCase {
                            "\n - The path must be either [1,2,3],  [1,2,7,5,3],  or  [1,6,7,5,3]");
         System.out.println("\tActual path found is: " + path);
         System.out.println("--------------------------------------------------------------------------");
-
-        //graph object to hold generated graph using BuildGraphFromDotFile
-        testGraph = GraphUtil.buildGraphFromDotFile("examplegraph6.dot");
-        List<String> path2 = GraphUtil.depthFirstSearch(testGraph, "1", "3");
-        System.out.println("--------------------------------------------------------------------------");
-        System.out.println("Using depthFirstSearch for examplegraph6, path from vertex \"1\" to \"3\"" +
-                           "\n - The path must be either [1,2,3],  [1,2,7,5,3],  or  [1,6,7,5,3]");
-        System.out.println("\tActual path found is: " + path2);
-        System.out.println("--------------------------------------------------------------------------");
     }
 
     public void testBreadthFirstSearch() throws Exception {
