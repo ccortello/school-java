a3e0842273fa38a238604485415ba54d5446ef4b
ccortello <cody.cortello@gmail.com>
2014-07-09 20:51:07 -0600

    moved assignment7 files and started assignment8

---
+++ CS 2420 - Summer 2014/src/assignment8/HashTable.java
@@ -0,0 +1,53 @@
+package assignment8;
+
+import java.util.Collection;
+
+/**
+ * An abstract class facilitating the implementation of a concrete hash table.
+ *
+ * @author Paymon Saebi
+ * @author
+ * @author
+ */
+public abstract class HashTable implements Set<String> {
+    /**
+     * FILL IN MEMBER VARIABLES!
+     *
+     * Any member variables that you would maintain in both your QuadProbeHashTable and
+     * your ChainingHashTable (such as, say, a size variable) probably belong here in
+     * the parent class. Should the variable(s) be public, private, or protected?
+     */
+    //TODO
+
+    /**
+     *
+     */
+    public final boolean addAll(Collection<? extends String> items) {
+        //TODO
+        return false;
+    }
+
+    /**
+     *
+     */
+    public final boolean containsAll(Collection<? extends String> items) {
+        //TODO
+        return false;
+    }
+
+    /**
+     *
+     */
+    public final boolean isEmpty() {
+        //TODO
+        return false;
+    }
+
+    /**
+     *
+     */
+    public final int size() {
+        //TODO
+        return 0;
+    }
+}
