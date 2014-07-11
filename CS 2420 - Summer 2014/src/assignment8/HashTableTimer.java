c3f41a368bf2c36520548fc6b9be4a0c383af441
caseyjames <casey.nordgran@gmail.com>
2014-07-10 23:32:13 -0600

    added some timing analysis implementation for the HashFunctors

--- CS 2420 - Summer 2014/src/assignment8/HashTableTimer.java
+++ CS 2420 - Summer 2014/src/assignment8/HashTableTimer.java
@@ -1,5 +1,7 @@
 package assignment8;
 
+import java.util.Random;
+
 /**
  * class to perform the timing analysis of assignment 8
  *
@@ -7,6 +9,95 @@ package assignment8;
  * @author Casey Nordgran
  */
 public class HashTableTimer {
+    static int MAX_TABLE_SIZE = 100000;
+    static int TIMES_TO_LOOP = 100;
+    static int INTERVAL = 5000;
+
+
+    public static void main(String[] args) {
+        hashFunctionTime();
+    }
+
+    public static void hashFunctionTime() {
+        long startTime, midTime, endTime;
+        int dataIndex = 0;
+        // stores values than prints at the end.
+        int[] sizeData = new int[MAX_TABLE_SIZE / INTERVAL + 1];
+        double[] timeData = new double[MAX_TABLE_SIZE / INTERVAL + 1];
+
+        //uncomment the hashfunction you want to test
+        /*HashFunctor hasher = new BadHashFunctor();*/
+        /*HashFunctor hasher = new FairHashFunctor();*/
+        HashFunctor hasher = new GoodHashFunctor();
+
+        // table sizes
+        for (int i = 0; i <= MAX_TABLE_SIZE; i += INTERVAL) {
+            if (i == 0) i = 1000;
+
+            int combined = i * TIMES_TO_LOOP;
+//            String[] randomStrings = stringArray(i * TIMES_TO_LOOP);
+            String[] randomStrings = stringArray(combined);
+            String temp;
+            int index;
+
+            // let a while loop run for a full second to get things spooled up.
+            startTime = System.nanoTime();
+            while (System.nanoTime() - startTime < 1e9) { //empty block
+            }
+
+            startTime = System.nanoTime();
+            index = 0;
+//            for (int j = 0; j < TIMES_TO_LOOP; j++) {
+            for (int k = 0; k < combined; k++) {
+                temp = randomStrings[index++];
+                // this next statement is what is dependent on the hashtable size.
+                int hashIndex = hasher.hash(temp) % i;
+            }
+//            }
+
+            midTime = System.nanoTime();
+            index = 0;
+//            for (int j = 0; j < TIMES_TO_LOOP; j++) {
+            for (int k = 0; k < combined; k++) {
+                temp = randomStrings[index++];
+            }
+//            }
+
+            endTime = System.nanoTime();
+            sizeData[dataIndex] = i;
+            timeData[dataIndex++] = (double) (2 * midTime - startTime - endTime) / combined;
+//            timeData[dataIndex++] = (double) (2 * midTime - startTime - endTime) / i;
+
+            if (i == 1000) i = 0;
+        }
+        // after testing finishes, print stored data
+        System.out.println("size\ttime");
+        for (int i = 0; i <= MAX_TABLE_SIZE / INTERVAL; i++) {
+            System.out.println(sizeData[i] + "\t" + timeData[i]);
+        }
+    }
+
+    public static String[] stringArray(int capacity) {
+        // method to create a string array of random strings made up of lowercase letters.
+
+        String[] array = new String[capacity];
+        Random rand = new Random(System.currentTimeMillis());
+
+        // first loop to iterate indexes in array
+        for (int i = 0; i < capacity; i++) {
+            // sets random string length at index 'i' of the string array, length from 4 to 20 characters
+            int stringLength = rand.nextInt(17) + 4;
+            // for loop adds random characters to string at index 'i' until j = stringLength-1
+            array[i] = "";
+            for (int j = 0; j < stringLength; j++) {
+                array[i] += Character.toString((char) (rand.nextInt(26) + 97));
+            }
+//            if (i%1000==0)
+//                System.out.println(array[i]);
+
+        }
+        return array;
+    }
 
 //     TODO: conduct an experiment assess the quality and efficiency of each of your three hash functions.
 //    * A recommendation for this experiment is to create two plots: one that shows the
