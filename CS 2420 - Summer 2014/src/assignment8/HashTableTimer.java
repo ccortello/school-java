de90990a24785bbb4ac728c7754f9bd382bce1a0
caseyjames <casey.nordgran@gmail.com>
2014-07-10 20:26:48 -0600

    added HashTableTimer class with //TODO: 's and his recomendations on how to do it.

---
+++ CS 2420 - Summer 2014/src/assignment8/HashTableTimer.java
@@ -0,0 +1,23 @@
+package assignment8;
+
+/**
+ * class to perform the timing analysis of assignment 8
+ *
+ * @author Cody Cortello
+ * @author Casey Nordgran
+ */
+public class HashTableTimer {
+
+//     TODO: conduct an experiment assess the quality and efficiency of each of your three hash functions.
+//    * A recommendation for this experiment is to create two plots: one that shows the
+//    growth of the number of collisions incurred by each hash function for a variety of
+//    hash table sizes, and one that shows the actual running time required by each hash
+//    function for a variety of hash table sizes. You may use either type of table for
+//    this experiment.
+
+//     TODO: conduct an experiment assess the quality and efficiency of each of your two hash tables.
+//    * A recommendation for this experiment is to create two plots: one that shows the number of
+//     collisions incurred by each hash table using the hash function in GoodHashFunctor, and one
+//     that shows the actual running time required by each hash table using the hash function in
+//     GoodHashFunctor.
+}
