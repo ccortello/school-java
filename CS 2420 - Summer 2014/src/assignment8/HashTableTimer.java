d1be6e4d71ed94d4382d141613443c5aa53f3526
ccortello <cody.cortello@gmail.com>
2014-07-12 18:25:19 -0600

    more work on timing, started table timing

--- CS 2420 - Summer 2014/src/assignment8/HashTableTimer.java
+++ CS 2420 - Summer 2014/src/assignment8/HashTableTimer.java
@@ -2,7 +2,6 @@ package assignment8;
 
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Random;
 
@@ -16,35 +15,40 @@ public class HashTableTimer {
     static int MAX_TABLE_SIZE = 35000;
     static int TIMES_TO_LOOP = 100;
     static int INTERVAL = 1000;
-//    static long seed = 82314917283974983L;
+	static boolean printAsRuns = true;
+	static boolean printAtEnd = false;
+	static boolean printToFile = false;
+	static long seed = 82314917283974983L;
 
     public static void main(String[] args) {
         try {
-            BadHashFunctionTime();
-            FairHashFunctionTime();
-            GoodHashFunctionTime();
-        } catch (Exception e) {
-            System.out.println(e.getMessage() + "\n" + e.getStackTrace());
-        }
+//            BadHashFunctionTime();
+//            FairHashFunctionTime();
+//            GoodHashFunctionTime();
+	        BadHashProbingTableTime();
+        } catch (Exception e) {System.out.println(e.getMessage() + "\n" + e.getStackTrace());}
     }
 
-    public static void BadHashFunctionTime() throws IOException {
-        long startTime, midTime, endTime;
+	public static void BadHashFunctionTime() throws Exception {
+		long startTime, midTime, endTime;
         int dataIndex = 0;
-        // stores values than prints at the end.
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("BadHashFunctionTime" +
+				".txt"), "utf-8"));
+
         int[] sizeData = new int[MAX_TABLE_SIZE / INTERVAL + 1];
         double[] totalTimeData = new double[MAX_TABLE_SIZE / INTERVAL + 1];
 
         HashFunctor hasher = new BadHashFunctor();
 
         // print header
-//        System.out.println("Timing trial for BadHashFunctor()\nHashes\tTotal time\tAverage Time");
-        BufferedWriter writer = null;
-        writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("BadHashFunctionTime.txt"), "utf-8"));
+		if (printAsRuns || printAtEnd)
+			System.out.println("Timing trial for BadHashFunctor()\nHashes\tTotal time (s)\tAverage Time (ns)");
+		if (printToFile)
+			writer.write("Timing trial for BadHashFunctor()\nHashes	Total time	Average Time");
 
         // table sizes
-        for (int i = 40000; i <= MAX_TABLE_SIZE; i += INTERVAL) {
-            if (i == 0) i = 1000;
+		for (int i = 0; i <= MAX_TABLE_SIZE; i += INTERVAL) {
+			if (i == 0) i = 1000;
 
             int combined = i * TIMES_TO_LOOP;
             String[] randomStrings = stringArray(combined);
@@ -68,55 +72,62 @@ public class HashTableTimer {
 
             midTime = System.nanoTime();
             index = 0;
-//            for (int j = 0; j < TIMES_TO_LOOP; j++) {
-            for (int k = 0; k < combined; k++) {
-                temp = randomStrings[index++];
-            }
-//            }
+			for (int k = 0; k < combined; k++)
+				temp = randomStrings[index++];
 
             endTime = System.nanoTime();
 
             // calculate the total time and the average time
-            double totalTime = (double) (2 * midTime - startTime - endTime);
-            double avgTime = totalTime / combined;
+			double totalTime = (double) (2 * midTime - startTime - endTime) / 1e9;
+			double avgTime = totalTime * 1e9 / combined;
 
             // store the times to be printed after execution completes
-//            sizeData[dataIndex] = i;
-//            totalTimeData[dataIndex] = totalTime;
-//            totalTimeData[dataIndex++] = avgTime;
+			if (printAtEnd) {
+				sizeData[dataIndex] = i;
+				totalTimeData[dataIndex] = totalTime;
+				totalTimeData[dataIndex++] = avgTime;
+			}
 
             // write the data to the created file
-            writer.write(i + "\t" + totalTime + "\t" + avgTime);
+			if (printToFile)
+				writer.write(i + "\t" + totalTime + "\t" + avgTime);
 
-            // uncomment this section to print the results as the test runs
-//            System.out.println(i + "\t" + totalTime + "\t" + avgTime);
+			// print the results as the test runs
+			if (printAsRuns)
+				System.out.println(i + "\t" + totalTime + "\t" + avgTime);
 
             // return i to 0 for the first loop
             if (i == 1000) i = 0;
         }
-        // after testing finishes, print stored data
-//        System.out.println("size\ttime");
-//        for (int i = 0; i <= MAX_TABLE_SIZE / INTERVAL; i++)
-//            System.out.println(sizeData[i] + "\t" + totalTimeData[i]);
-    }
+		System.out.println("Finished BadHashFunction timing!\n");
+		// after testing finishes, print stored data
+		if (printAtEnd) {
+			System.out.println("size\ttime");
+			for (int i = 0; i <= MAX_TABLE_SIZE / INTERVAL; i++)
+				System.out.println(sizeData[i] + "\t" + totalTimeData[i]);
+		}
+	}
 
-    public static void FairHashFunctionTime() throws IOException {
-        long startTime, midTime, endTime;
+	public static void FairHashFunctionTime() throws Exception {
+		long startTime, midTime, endTime;
         int dataIndex = 0;
-        // stores values than prints at the end.
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("FairHashFunctionTime" +
+				".txt"), "utf-8"));
+
         int[] sizeData = new int[MAX_TABLE_SIZE / INTERVAL + 1];
         double[] totalTimeData = new double[MAX_TABLE_SIZE / INTERVAL + 1];
 
         HashFunctor hasher = new FairHashFunctor();
 
         // print header
-//        System.out.println("Timing trial for BadHashFunctor()\nHashes\tTotal time\tAverage Time");
-        BufferedWriter writer = null;
-        writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("FairHashFunctionTime.txt"), "utf-8"));
+		if (printAsRuns || printAtEnd)
+			System.out.println("Timing trial for FairHashFunctor()\nHashes\tTotal time (s)\tAverage Time (ns)");
+		if (printToFile)
+			writer.write("Timing trial for BadHashFunctor()\nHashes	Total time	Average Time");
 
         // table sizes
-        for (int i = 40000; i <= MAX_TABLE_SIZE; i += INTERVAL) {
-            if (i == 0) i = 1000;
+		for (int i = 0; i <= MAX_TABLE_SIZE; i += INTERVAL) {
+			if (i == 0) i = 1000;
 
             int combined = i * TIMES_TO_LOOP;
             String[] randomStrings = stringArray(combined);
@@ -140,55 +151,62 @@ public class HashTableTimer {
 
             midTime = System.nanoTime();
             index = 0;
-//            for (int j = 0; j < TIMES_TO_LOOP; j++) {
-            for (int k = 0; k < combined; k++) {
-                temp = randomStrings[index++];
-            }
-//            }
+			for (int k = 0; k < combined; k++)
+				temp = randomStrings[index++];
 
             endTime = System.nanoTime();
 
             // calculate the total time and the average time
-            double totalTime = (double) (2 * midTime - startTime - endTime);
-            double avgTime = totalTime / combined;
+			double totalTime = (double) (2 * midTime - startTime - endTime) / 1e9;
+			double avgTime = totalTime * 1e9 / combined;
 
             // store the times to be printed after execution completes
-//            sizeData[dataIndex] = i;
-//            totalTimeData[dataIndex] = totalTime;
-//            totalTimeData[dataIndex++] = avgTime;
+			if (printAtEnd) {
+				sizeData[dataIndex] = i;
+				totalTimeData[dataIndex] = totalTime;
+				totalTimeData[dataIndex++] = avgTime;
+			}
 
             // write the data to the created file
-            writer.write(i + "\t" + totalTime + "\t" + avgTime);
+			if (printToFile)
+				writer.write(i + "\t" + totalTime + "\t" + avgTime);
 
-            // uncomment this section to print the results as the test runs
-//            System.out.println(i + "\t" + totalTime + "\t" + avgTime);
+			// print the results as the test runs
+			if (printAsRuns)
+				System.out.println(i + "\t" + totalTime + "\t" + avgTime);
 
             // return i to 0 for the first loop
             if (i == 1000) i = 0;
         }
-        // after testing finishes, print stored data
-//        System.out.println("size\ttime");
-//        for (int i = 0; i <= MAX_TABLE_SIZE / INTERVAL; i++)
-//            System.out.println(sizeData[i] + "\t" + totalTimeData[i]);
-    }
+		System.out.println("Finished FairHashFunction timing!\n");
+		// after testing finishes, print stored data
+		if (printAtEnd) {
+			System.out.println("size\ttime");
+			for (int i = 0; i <= MAX_TABLE_SIZE / INTERVAL; i++)
+				System.out.println(sizeData[i] + "\t" + totalTimeData[i]);
+		}
+	}
 
-    public static void GoodHashFunctionTime() throws IOException {
-        long startTime, midTime, endTime;
+	public static void GoodHashFunctionTime() throws Exception {
+		long startTime, midTime, endTime;
         int dataIndex = 0;
-        // stores values than prints at the end.
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("GoodHashFunctionTime" +
+				".txt"), "utf-8"));
+
         int[] sizeData = new int[MAX_TABLE_SIZE / INTERVAL + 1];
         double[] totalTimeData = new double[MAX_TABLE_SIZE / INTERVAL + 1];
 
         HashFunctor hasher = new GoodHashFunctor();
 
         // print header
-//        System.out.println("Timing trial for BadHashFunctor()\nHashes\tTotal time\tAverage Time");
-        BufferedWriter writer = null;
-        writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("GoodHashFunctionTime.txt"), "utf-8"));
+		if (printAsRuns || printAtEnd)
+			System.out.println("Timing trial for GoodHashFunctor()\nHashes\tTotal time (s)\tAverage Time (ns)");
+		if (printToFile)
+			writer.write("Timing trial for BadHashFunctor()\nHashes	Total time	Average Time");
 
         // table sizes
-        for (int i = 40000; i <= MAX_TABLE_SIZE; i += INTERVAL) {
-            if (i == 0) i = 1000;
+		for (int i = 0; i <= MAX_TABLE_SIZE; i += INTERVAL) {
+			if (i == 0) i = 1000;
 
             int combined = i * TIMES_TO_LOOP;
             String[] randomStrings = stringArray(combined);
@@ -212,37 +230,41 @@ public class HashTableTimer {
 
             midTime = System.nanoTime();
             index = 0;
-//            for (int j = 0; j < TIMES_TO_LOOP; j++) {
-            for (int k = 0; k < combined; k++) {
-                temp = randomStrings[index++];
-            }
-//            }
+			for (int k = 0; k < combined; k++)
+				temp = randomStrings[index++];
 
             endTime = System.nanoTime();
 
             // calculate the total time and the average time
-            double totalTime = (double) (2 * midTime - startTime - endTime);
-            double avgTime = totalTime / combined;
+			double totalTime = (double) (2 * midTime - startTime - endTime) / 1e9;
+			double avgTime = totalTime * 1e9 / combined;
 
             // store the times to be printed after execution completes
-//            sizeData[dataIndex] = i;
-//            totalTimeData[dataIndex] = totalTime;
-//            totalTimeData[dataIndex++] = avgTime;
+			if (printAtEnd) {
+				sizeData[dataIndex] = i;
+				totalTimeData[dataIndex] = totalTime;
+				totalTimeData[dataIndex++] = avgTime;
+			}
 
             // write the data to the created file
-            writer.write(i + "\t" + totalTime + "\t" + avgTime);
+			if (printToFile)
+				writer.write(i + "\t" + totalTime + "\t" + avgTime);
 
-            // uncomment this section to print the results as the test runs
-//            System.out.println(i + "\t" + totalTime + "\t" + avgTime);
+			// print the results as the test runs
+			if (printAsRuns)
+				System.out.println(i + "\t" + totalTime + "\t" + avgTime);
 
             // return i to 0 for the first loop
-            if (i == 1000) i = 0;
+//            if (i == 1000) i = 0;
         }
-        // after testing finishes, print stored data
-//        System.out.println("size\ttime");
-//        for (int i = 0; i <= MAX_TABLE_SIZE / INTERVAL; i++)
-//            System.out.println(sizeData[i] + "\t" + totalTimeData[i]);
-    }
+		System.out.println("Finished GoodHashFunction timing!\n");
+		// after testing finishes, print stored data
+		if (printAtEnd) {
+			System.out.println("size\ttime");
+			for (int i = 0; i <= MAX_TABLE_SIZE / INTERVAL; i++)
+				System.out.println(sizeData[i] + "\t" + totalTimeData[i]);
+		}
+	}
 
     public static String[] stringArray(int capacity) {
 
@@ -264,12 +286,90 @@ public class HashTableTimer {
         return array;
     }
 
+	public static void BadHashProbingTableTime() throws Exception {
+		long startTime, midTime, endTime;
+		int dataIndex = 0;
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("BadHashFunctionTime.txt"), "utf-8"));
+
+		int[] sizeData = new int[MAX_TABLE_SIZE / INTERVAL + 1];
+		double[] totalTimeData = new double[MAX_TABLE_SIZE / INTERVAL + 1];
+
+		HashFunctor hasher = new BadHashFunctor();
+
+		// print header
+		if (printAsRuns || printAtEnd)
+			System.out.println("Timing trial for BadHashProbingTableTime\nHashes\tTotal time (s)\tAverage Time (ns)");
+		if (printToFile)
+			writer.write("Timing trial for BadHash with ProbingTable()\nHashes	Total time	Average Time");
+
+		// table sizes
+		for (int i = 0; i <= MAX_TABLE_SIZE; i += INTERVAL) {
+			if (i == 0) i = 1000;
+
+			int combined = i * TIMES_TO_LOOP;
+			String[] randomStrings = stringArray(combined);
+			String temp;
+			int index;
+
+			// let a while loop run for a full second to get things spooled up.
+			startTime = System.nanoTime();
+			while (System.nanoTime() - startTime < 1e9) { //empty block
+			}
+
+			startTime = System.nanoTime();
+			index = 0;
+//            for (int j = 0; j < TIMES_TO_LOOP; j++) {
+			for (int k = 0; k < combined; k++) {
+				temp = randomStrings[index++];
+				// this next statement is what is dependent on the hashtable size.
+				int hashIndex = hasher.hash(temp) % i;
+			}
+//            }
+
+			midTime = System.nanoTime();
+			index = 0;
+			for (int k = 0; k < combined; k++)
+				temp = randomStrings[index++];
+
+			endTime = System.nanoTime();
+
+			// calculate the total time and the average time
+			double totalTime = (double) (2 * midTime - startTime - endTime) / 1e9;
+			double avgTime = totalTime * 1e9 / combined;
+
+			// store the times to be printed after execution completes
+			if (printAtEnd) {
+				sizeData[dataIndex] = i;
+				totalTimeData[dataIndex] = totalTime;
+				totalTimeData[dataIndex++] = avgTime;
+			}
+
+			// write the data to the created file
+			if (printToFile)
+				writer.write(i + "\t" + totalTime + "\t" + avgTime);
+
+			// print the results as the test runs
+			if (printAsRuns)
+				System.out.println(i + "\t" + totalTime + "\t" + avgTime);
+
+			// return i to 0 for the first loop
+			if (i == 1000) i = 0;
+		}
+		System.out.println("Finished BadHashFunction timing!\n");
+		// after testing finishes, print stored data
+		if (printAtEnd) {
+			System.out.println("size\ttime");
+			for (int i = 0; i <= MAX_TABLE_SIZE / INTERVAL; i++)
+				System.out.println(sizeData[i] + "\t" + totalTimeData[i]);
+		}
+	}
 //     TODO: conduct an experiment assess the quality and efficiency of each of your three hash functions.
 //    * A recommendation for this experiment is to create two plots: one that shows the
 //    growth of the number of collisions incurred by each hash function for a variety of
 //    hash table sizes, and one that shows the actual running time required by each hash
 //    function for a variety of hash table sizes. You may use either type of table for
 //    this experiment.
+
 
 //     TODO: conduct an experiment assess the quality and efficiency of each of your two hash tables.
 //    * A recommendation for this experiment is to create two plots: one that shows the number of
