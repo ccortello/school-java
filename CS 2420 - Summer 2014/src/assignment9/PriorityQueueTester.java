e4e83cb8e66e6ff2a8304b905f12a865981708bb
caseyjames <casey.nordgran@gmail.com>
2014-07-18 22:04:56 -0600

    some implementation to the Tester class

--- CS 2420 - Summer 2014/src/assignment9/PriorityQueueTester.java
+++ CS 2420 - Summer 2014/src/assignment9/PriorityQueueTester.java
@@ -11,6 +11,13 @@ public class PriorityQueueTester extends TestCase {
      * Tests the methods of the PriorityQueueHEAP class
      */
     public void testPriorityQueueHEAP() {
+        // create a PQ of type Integer to test
+        PriorityQueueHEAP<Integer> testPQ = new PriorityQueueHEAP<Integer>();
+        // before adding, assert the size is zero
+        assertEquals(0, testPQ.size());
+        // add Integers to th PQ, and test min value
+        testPQ.add(1);
+        assertEquals((Integer) 1, testPQ.findMin());
 
     }
 
