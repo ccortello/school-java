b2ca09cd48e08127d747365f8f14cade8f3f4b55
ccortello <cody.cortello@gmail.com>
2014-07-20 21:25:31 -0600

    finished assignment9!

--- CS 2420 - Summer 2014/src/assignment9/PriorityHeapTimer.java
+++ CS 2420 - Summer 2014/src/assignment9/PriorityHeapTimer.java
@@ -11,7 +11,7 @@ public class PriorityHeapTimer {
     static boolean printAtEnd = false;
     static int MAX_SIZE = Integer.MAX_VALUE;
     static int INTERVAL = 5000;
-    static int TIMES_TO_LOOP = 100;
+    static int TIMES_TO_LOOP = 200;
 
     public static void main(String[] args) {
         new PriorityHeapTimer();
@@ -21,7 +21,8 @@ public class PriorityHeapTimer {
         try {
 //            PriorityHeapAddTimer();
 //            PriorityHeapFindMinTimer();
-            PriorityHeapDelMinTimer();
+//            PriorityHeapDelMinTimer();
+            PriorityBSTAddTimer();
         } catch (Exception e) {
             System.out.println(e.getMessage() + "\n" + e.getStackTrace());
         }
@@ -229,6 +230,77 @@ public class PriorityHeapTimer {
 //			if (currentSize == 1000) currentSize = 0;
         }
         System.out.println("Finished PQHEAP delMin() timing!\n");
+        // after testing finishes, print stored data
+        if (printAtEnd) {
+            System.out.println("size\ttime");
+            for (int i = 0; i <= MAX_SIZE / INTERVAL; i++)
+                System.out.println(sizeData[i] + "\t" + totalTimeData[i]);
+        }
+    }
+
+    public static void PriorityBSTAddTimer() throws Exception {
+        long startTime, midTime, endTime;
+        int dataIndex = 0;
+
+        int[] sizeData = new int[MAX_SIZE / INTERVAL + 1];
+        double[] totalTimeData = new double[MAX_SIZE / INTERVAL + 1];
+
+        PriorityQueueBST<Integer> bst = new PriorityQueueBST<Integer>();
+
+        // print header
+        if (printAsRuns || printAtEnd)
+            System.out.println("Timing trial for PriorityQueueBST add()\nAdds\tTotal time (s)\tAverage Time (ns)");
+
+        // table sizes
+        for (int currentSize = INTERVAL; currentSize <= MAX_SIZE; currentSize += INTERVAL) {
+//			if (currentSize == 0) currentSize = 1000;
+
+            // initiate variables for timing structures
+            double combined = currentSize * TIMES_TO_LOOP; // represents the number of total operations which should be timed
+            Random rand = new Random(seed);
+
+            // let a while loop run for a full second to get things spooled up.
+            startTime = System.nanoTime();
+            while (System.nanoTime() - startTime < 1e9) { //empty block
+            }
+
+            startTime = System.nanoTime();
+            for (int j = 0; j < TIMES_TO_LOOP; j++) {
+                bst = new PriorityQueueBST<Integer>();
+                for (int k = 0; k < currentSize; k++) {
+                    bst.add(rand.nextInt());
+                }
+            }
+
+            midTime = System.nanoTime();
+            for (int j = 0; j < TIMES_TO_LOOP; j++) {
+                bst = new PriorityQueueBST<Integer>();
+                for (int k = 0; k < currentSize; k++) {
+                }
+            }
+
+            endTime = System.nanoTime();
+
+            // calculate the total time and the average time
+            double totalTime = (double) (2 * midTime - startTime - endTime) / 1e9;
+            double avgTime = totalTime * 1e9 / combined;
+
+            // store the times to be printed after execution completes
+            if (printAtEnd) {
+                sizeData[dataIndex] = currentSize;
+                totalTimeData[dataIndex] = totalTime;
+                totalTimeData[dataIndex++] = avgTime;
+            }
+
+            // print the results as the test runs
+            if (printAsRuns)
+                System.out.println(currentSize + "\t" + totalTime + "\t" + avgTime);
+
+//			return currentSize to 0 for the first loop
+//			if (currentSize == 1000) currentSize = 0;
+        }
+        System.out.println("Finished PQBST add() timing!\n");
+
         // after testing finishes, print stored data
         if (printAtEnd) {
             System.out.println("size\ttime");
